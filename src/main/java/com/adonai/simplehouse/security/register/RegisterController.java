69a99f588e5ddfaf53aa7efa599e40e8c1bb2c5a
Geovane Araújo <geovane-araujo@outlook.com>
2022-02-08 23:55:10 -0300

    Criação do register

--- src/main/java/com/adonai/simplehouse/security/register/RegisterController.java
+++ src/main/java/com/adonai/simplehouse/security/register/RegisterController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -16,7 +17,7 @@ public class RegisterController {
     RegisterService registerService;
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(Users user){
+    public ResponseEntity<?> register(@RequestBody Users user){
         registerService.onSaveRegister(user);
         return ResponseEntity.ok().body("OK");
     }
--- src/main/java/com/adonai/simplehouse/util/GlobalVariables.java
+++ src/main/java/com/adonai/simplehouse/util/GlobalVariables.java
@@ -2,5 +2,5 @@ package com.adonai.simplehouse.util;
 
 public class GlobalVariables {
 
-    public static String dbName = "adonai_info-simple_house/";
+    public static String dbName = "adonai_info-simple_house?currentSchema=";
 }
