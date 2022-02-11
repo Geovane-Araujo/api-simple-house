14cd98d3f09de1a99d79f9034e79ffb9f4f1f79b
Geovane Araújo <geovane-araujo@outlook.com>
2022-02-11 00:10:05 -0300

    configuração email

--- src/main/java/com/adonai/simplehouse/security/register/RegisterService.java
+++ src/main/java/com/adonai/simplehouse/security/register/RegisterService.java
@@ -34,8 +34,10 @@ public class RegisterService {
 
             helper.setFrom("noreply@adonaisoft.com.br");
             helper.setSubject("Olá seja Bem vindo");
-            helper.setText("<h1>Thunai</h1>",true);
+            helper.setText(onTextHtml("5565654"),true);
             helper.setTo(user.getEmail());
+            //mail.setContent(onTextHtml("5565654"),"text/html");
+            mail.saveChanges();
             mailSender.send(mail);
 
         } catch (MessagingException e) {
@@ -45,5 +47,107 @@ public class RegisterService {
 
     public void onCreatedTenant(Users user){
 
+    }
+
+    public String onTextHtml(String code){
+        String html = "<head>\n" +
+                "  <style>\n" +
+                "    body{\n" +
+                "      height: 100vh;\n" +
+                "      width: 100vw;\n" +
+                "      background-color: #E5E5E5;\n" +
+                "      margin: 0;\n" +
+                "      font-family: 'Roboto', sans-serif;\n" +
+                "    }\n" +
+                "    .body{\n" +
+                "      display: flex;\n" +
+                "      justify-content: center;\n" +
+                "      align-items: center;\n" +
+                "      height: 100%;\n" +
+                "      width: 100%;\n" +
+                "    }\n" +
+                "    .card-header{\n" +
+                "      height: 95vh;\n" +
+                "      width: 700px;\n" +
+                "      background-color: aliceblue;\n" +
+                "    }\n" +
+                "    .top{\n" +
+                "      width: 100%;\n" +
+                "      height: 30%;\n" +
+                "      margin: 0;\n" +
+                "      background-color: #FFE6E1;\n" +
+                "      justify-content: left;\n" +
+                "      align-items: center;\n" +
+                "      display: flex;\n" +
+                "      \n" +
+                "    }\n" +
+                "    .center{\n" +
+                "      width: 100%;\n" +
+                "      height: 60%;\n" +
+                "      background-color: #FFE6E1;\n" +
+                "      display: flex;\n" +
+                "      flex-direction: column;\n" +
+                "      justify-content: center;\n" +
+                "      align-items: center;\n" +
+                "    }\n" +
+                "    .text{\n" +
+                "      font-size: 30px;\n" +
+                "      margin-left: 45px;\n" +
+                "    }\n" +
+                "    .botton{\n" +
+                "      display: flex;\n" +
+                "      align-items: center;\n" +
+                "      width: 100%;\n" +
+                "      height: 10%;\n" +
+                "    }\n" +
+                "    .code{\n" +
+                "      height: 40px;\n" +
+                "      width: 200px;\n" +
+                "      background-color: bisque;\n" +
+                "      display: flex;\n" +
+                "      justify-content: center;\n" +
+                "      align-items: center;\n" +
+                "    }\n" +
+                "    .text-center{\n" +
+                "      font-size: 25px;\n" +
+                "      margin: 0px 20px 60px 20px;\n" +
+                "      text-align: center;\n" +
+                "    }\n" +
+                "  </style>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "  <div class=\"body\">\n" +
+                "    <div class=\"card-header\">\n" +
+                "      <div class=\"top\">\n" +
+                "        <div style=\"margin-left: 20px;\">\n" +
+                "          <img src=\"https://arquivos.adonaisoft.com.br/images/logo-simple.png\" alt=\"\">\n" +
+                "          <h3 class=\"text\">Olá este é o seu código de confirmação</h3>\n" +
+                "        </div>\n" +
+                "        \n" +
+                "      </div>\n" +
+                "      <div class=\"center\">\n" +
+                "        \n" +
+                "        <div class=\"text-center\">\n" +
+                "          Olá Ficamos felizes em saber que decidiu usar o app para ajudar na organização de sua casa\n" +
+                "          <p>Abaixo está o código para ativação da conta, insira-o no app:</p>\n" +
+                "        </div>\n" +
+                "        <div class=\"code\">\n" +
+                "            <h1>@@code</h1>\n" +
+                "        </div>\n" +
+                "        \n" +
+                "      </div>\n" +
+                "      <div class=\"botton\">\n" +
+                "        <div style=\"margin-left: 40px;\">\n" +
+                "          2022 - Todos os direitos reservados a AdonaiSoft\n" +
+                "          conheça nossos produtos <a href=\"https:www.adonaisoft.com.br\">Aqui</a>\n" +
+                "        </div>\n" +
+                "      </div>\n" +
+                "    </div>\n" +
+                "  </div>\n" +
+                "</body>";
+
+
+
+        return html.replace("@@code",code);
     }
 }
